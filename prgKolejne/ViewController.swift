d5d8aebe1b8da643e2e04c0ac34e67142249ab18
piotr wojciechowski <piowo1@o2.l>
2020-03-19 07:52:49 +0100

    Initial Commit

---
+++ prgKolejne/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  prgKolejne
+//
+//  Created by Piotr Wojciechowski on 19/03/2020.
+//  Copyright © 2020 Piotr Wojciechowski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
